5c21105fad15ad4ec8ff00d8734efc816e4ac13c
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-04-20 15:19:23 +0100

    add loading message

--- src/components/Post-List.vue
+++ src/components/Post-List.vue
@@ -1,5 +1,8 @@
 <template>
-  <section>
+  <section v-if="loading">
+    <h1>Loading...</h1>
+  </section>
+  <section v-else>
     <h1 v-for="post in posts[index]" :key="post.id">
       <a :href="post.url" target="_blank" rel="noreferrer">{{ post.title }}</a>
     </h1>
@@ -12,14 +15,23 @@ import hn from '../clients/hn'
 
 export default {
   name: 'PostList',
-  data: function () { return { posts: [], index: 0 } },
+  data: function () {
+    return {
+      loading: true,
+      posts: [],
+      index: 0
+    }
+  },
   mounted: async function () {
     this.posts = await hn.posts(this.index)
+    this.loading = false
   },
   methods: {
     viewMore: async function () {
+      this.loading = true
       this.index = this.index + 1
       this.posts = await hn.posts(this.index)
+      this.loading = false
     }
   }
 }
