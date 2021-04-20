e760c41231562f57f018ca7b419af18c8da4b411
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-04-20 14:00:15 +0100

    remove comment

--- src/clients/hn.js
+++ src/clients/hn.js
@@ -7,8 +7,6 @@ export default {
 
     const posts = chunk(data, 30)
 
-    // https://hacker-news.firebaseio.com/v0/item/26871558.json
-
     const promises = posts[0].map(async (post) => {
       console.log('p', post)
       const postRes = await window.fetch(`https://hacker-news.firebaseio.com/v0/item/${post}.json`)
