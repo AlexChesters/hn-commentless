945c1d06fd6d755beeee42a6e28d988f04cc0d17
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-04-20 15:16:22 +0100

    remove ask hn posts

--- src/clients/hn.js
+++ src/clients/hn.js
@@ -11,12 +11,14 @@ export default {
 
     const posts = chunk(data, 30)
 
-    const promises = posts[index].map(async (post) => {
-      const postRes = await window.fetch(`https://hacker-news.firebaseio.com/v0/item/${post}.json`)
-      return postRes.json()
-    })
+    const promises = posts[index]
+      .map(async (post) => {
+        const postRes = await window.fetch(`https://hacker-news.firebaseio.com/v0/item/${post}.json`)
+        return postRes.json()
+      })
 
     posts[index] = await Promise.all(promises)
+    posts[index] = posts[index].filter(({ url }) => url)
 
     return posts
   }
